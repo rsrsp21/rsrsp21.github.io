001bef016493046a6c59d53d1d55fcd24d4a536d
Sri Ram Sai Pavan Relangi <107051446+rsrsp21@users.noreply.github.com>
2023-07-07 12:08:00 +0530

    Update script2_1_2021.js

--- 2021_Results/script2_1_2021.js
+++ 2021_Results/script2_1_2021.js
@@ -3665,55 +3665,89 @@ var csvData = `ID,Subject,Grade,Credits
     }
 
     
-    function displayResults() {
-      var studentId = document.getElementById('student-id').value.trim();
-      if (!studentId) {
-        alert('Please enter a valid Roll Number');
-        return;
-      }
+    var clearedSupplementaryIDs = []
 
-      var studentData = getStudentData(studentId, parseCSV(csvData));
-      if (studentData.length === 0) {
-        alert('No data found for the given Roll Number.');
-        return;
-      }
+function displayResults() {
+var studentId = document.getElementById('student-id').value.trim();
+  if (!studentId) {
+    alert('Please enter a valid Roll Number');
+    return;
+  }
 
-      var resultsContainer = document.getElementById('results-container');
-      resultsContainer.innerHTML = '';
+  var studentData = getStudentData(studentId, parseCSV(csvData));
+  if (studentData.length === 0) {
+    alert('No data found for the given Roll Number.');
+    return;
+  }
 
-      var table = document.createElement('table');
-      var tableHeader = document.createElement('thead');
-      var tableBody = document.createElement('tbody');
+ var idContainer = document.getElementById('id-container');
+  var idHeading = idContainer.querySelector('p');
+  idHeading.textContent = 'Roll Number: ';
+ idHeading.style.color = 'black';
+  idHeading.style.fontWeight = 'bold';
+  idContainer.style.marginTop = '20px';
 
-      var headers = Object.keys(studentData[0]);
-      var headerRow = document.createElement('tr');
-      headers.forEach(function(header) {
-        var th = document.createElement('th');
-        th.textContent = header;
-        headerRow.appendChild(th);
-      });
-      tableHeader.appendChild(headerRow);
+  var idValue = document.createElement('span');
+  idValue.textContent = studentId;
+  idValue.style.color = 'red';
+  idValue.style.fontWeight = 'bold';
+  idHeading.appendChild(idValue);
 
-      studentData.forEach(function(subject) {
-        var row = document.createElement('tr');
-        Object.values(subject).forEach(function(value) {
-          var td = document.createElement('td');
-          td.textContent = value;
-          row.appendChild(td);
-        });
-        tableBody.appendChild(row);
-      });
+  var resultsContainer = document.getElementById('results-container');
+  resultsContainer.innerHTML = '';
 
-      table.appendChild(tableHeader);
-      table.appendChild(tableBody);
-      resultsContainer.appendChild(table);
+  var table = document.createElement('table');
+  var tableHeader = document.createElement('thead');
+  var tableBody = document.createElement('tbody');
 
-      var sgpa = calculateSGPA(studentData);
-
-      var sgpaContainer = document.querySelector('.sgpa');
-      sgpaContainer.textContent = 'SGPA: ' + sgpa;
+  var headers = Object.keys(studentData[0]);
+  var headerRow = document.createElement('tr');
+  headers.forEach(function(header) {
+    if (header !== 'ID') {
+      var th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
     }
-	function handleKeyPress(event) {
+  });
+  tableHeader.appendChild(headerRow);
+
+  studentData.forEach(function(subject) {
+    var row = document.createElement('tr');
+    Object.entries(subject).forEach(function([key, value]) {
+      if (key !== 'ID') {
+        var td = document.createElement('td');
+        td.textContent = value;
+        row.appendChild(td);
+      }
+    });
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableHeader);
+  table.appendChild(tableBody);
+  resultsContainer.appendChild(table);
+	
+  var sgpaContainer = document.getElementById('sgpa-container');
+  sgpaContainer.innerHTML = '';
+
+  var sgpaResult = document.createElement('h3');
+  var sgpa = calculateSGPA(studentData);
+  sgpaResult.innerHTML = '<span style="color: black;">SGPA : </span><span style="color: red;">' + sgpa + '</span>';
+
+  var supplementaryResult = document.createElement('p');
+  if (sgpa === 'Fail') {
+    supplementaryResult.innerHTML = '<span style="color: blue;">Better luck next time!</span>';
+  } else if (clearedSupplementaryIDs.includes(studentId)) {
+    supplementaryResult.innerHTML = '<span style="color: blue;">Passed. Cleared in supplementary appearance(s).</span>';
+  } else {
+    supplementaryResult.innerHTML = '<span style="color: green;">Congratulations! You have passed!</span>';
+  }
+
+  sgpaContainer.appendChild(sgpaResult);
+  sgpaContainer.appendChild(supplementaryResult);
+document.getElementById('student-id').focus();
+}
+ function handleKeyPress(event) {
       	if (event.keyCode === 13) { // 13 represents the Enter key
         	displayResults();
       	}
@@ -3721,6 +3755,13 @@ var csvData = `ID,Subject,Grade,Credits
 
     	// Add event listener to input element
     	document.getElementById('student-id').addEventListener('keyup', handleKeyPress);
-	function printResults() {
-      window.print();
-    }
+
+function printResults() {
+  var printContents = document.querySelector('.container').innerHTML;
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+  window.print();
+
+  document.body.innerHTML = originalContents;
+}
