c62ddf3a79bdfe153665d938c2a35b88235f6e9d
Sri Ram Sai Pavan Relangi <107051446+rsrsp21@users.noreply.github.com>
2023-07-10 23:42:58 +0530

    Update 2021_script_cgpa.js

--- 2021_Results/2021_script_cgpa.js
+++ 2021_Results/2021_script_cgpa.js
@@ -429,9 +429,9 @@ function displayResults() {
   var resultsContainer = document.getElementById('results-container');
   resultsContainer.innerHTML = '';
 
-  var idHeading = document.createElement('h4');
+  var idHeading = document.createElement('h3');
   idHeading.style.textAlign = 'center';
-  idHeading.innerHTML = '<span style="color: black; font-weight: bold">ID: </span><span style="color: red; font-weight: bold">' + studentId + '</span>';
+  idHeading.innerHTML = '<span style="color: black; font-weight: bold">Roll Number : </span><span style="color: red; font-weight: bold">' + studentId + '</span>';
   resultsContainer.appendChild(idHeading);
 
   var table = document.createElement('table');
@@ -459,7 +459,7 @@ function displayResults() {
 
   var cgpaHeading = document.createElement('h3');
   cgpaHeading.style.textAlign = 'center';
-  cgpaHeading.innerHTML = '<span style="color: black; font-weight: bold">CGPA: </span><span style="color: red; font-weight: bold">' + studentData[0]['CGPA'] + '</span>';
+  cgpaHeading.innerHTML = '<span style="color: black; font-weight: bold">CGPA : </span><span style="color: red; font-weight: bold">' + studentData[0]['CGPA'] + '</span>';
   resultsContainer.appendChild(cgpaHeading);
 }
 
